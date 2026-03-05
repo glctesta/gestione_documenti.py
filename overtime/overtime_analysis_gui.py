4824f783d384522df0c21416602456fd66496246
glctesta <gtesta@gmail.com>
2026-03-05 17:04:06 +0200

    fix: exclude employees with FunctionCode > 60 from overtime analysis

--- overtime/overtime_analysis_gui.py
+++ overtime/overtime_analysis_gui.py
@@ -209,6 +209,9 @@ class OvertimeAnalysisWindow(tk.Toplevel):
                 ON ee.EmployeeId = h.EmployeeId
                 AND h.employeerid = 2
                 AND h.EndWorkDate IS NULL
+            LEFT JOIN Employee.dbo.Functions f
+                ON h.FunctionId = f.FunctionId
+            WHERE ISNULL(f.FunctionCode, 0) <= 60
         ),
         CTE_ExtraTimeApprovalStory AS (
             SELECT
